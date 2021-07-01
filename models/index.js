20e81bdb2be7c991dbc39e63f80a170471f1c3ce
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-07-01 11:17:03 -0700

    Added README

--- models/index.js
+++ models/index.js
@@ -2,25 +2,48 @@
 const mongoose = require('mongoose')
 require ('dotenv').config()
 
-// Define Atlas URI
+const connect = () => {
+    / Define Atlas URI
 const uri = process.env.ATLAS_URI
 
 // Connect Mongoose to Atlas
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
 
-const db = mongoose.connection
+    const db = mongoose.connection
 
-db.once('open', () => {
-    console.log(`⛓ mongoDB connected on $(db.host):$(db.port)`)
-})
+    db.once('open', () => {
+        console.log(`⛓ mongoDB connected on $(db.host):$(db.port)`)
+    })
 
-db.on('error', err => {
-    console.log(`🔥 Oh no! The database burned down!\. ${err}`)
+    db.on('error', err => {
+        console.log(`🔥 Oh no! The database burned down!\. ${err}`)
+    })
+    }
+
+    // Define Atlas URI
+    const uri = process.env.ATLAS_URI
+
+    // Connect Mongoose to Atlas
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+
+    const db = mongoose.connection
+
+    db.once('open', () => {
+        console.log(`⛓ mongoDB connected on $(db.host):$(db.port)`)
+    })
+
+    db.on('error', err => {
+        console.log(`🔥 Oh no! The database burned down!\. ${err}`)
 })
 
 // Export function to Connect
