16e264d058d2b22d97a54f0ee9dc2ade3a45c2ad
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-07-01 11:12:03 -0700

    Initial DB Setup

---
+++ models/index.js
@@ -0,0 +1,26 @@
+// Require Mongoose Package
+const mongoose = require('mongoose')
+require ('dotenv').config()
+
+// Define Atlas URI
+const uri = process.env.ATLAS_URI
+
+// Connect Mongoose to Atlas
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+const db = mongoose.connection
+
+db.once('open', () => {
+    console.log(`⛓ mongoDB connected on $(db.host):$(db.port)`)
+})
+
+db.on('error', err => {
+    console.log(`🔥 Oh no! The database burned down!\. ${err}`)
+})
+
+// Export function to Connect
